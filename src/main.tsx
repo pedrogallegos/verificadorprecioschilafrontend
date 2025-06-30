27ce771f49ac3a5a2ed4d7e019497a39b09d3236
pedrogallegos <pedrogallegos543@gmail.com>
2025-06-30 11:53:34 -0600

    feat: Implementar Dashboard POS como diseño principal
    - Crear LayoutSelector para probar diferentes diseños
    - Configurar AppPOS como layout principal
    - Diseño profesional tipo Point of Sale
    - Interface comercial optimizada para verificador de precios
    - Sidebar con navegación, estadísticas y cards informativos

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import AppPOS from './AppPOS.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppPOS />
   </StrictMode>,
 )
